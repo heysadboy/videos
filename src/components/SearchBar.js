e0db2407bec58839a08df88ff0af9b8386b80ae0
Varun Kumar <heysadboy@gmail.com>
2021-01-17 23:47:23 +0900

    Making an api request to youtube

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -11,9 +11,7 @@ class SearchBar extends React.Component {
 
     onFormSubmit = event => {
         event.preventDefault();
-
-        // TODO: Make sure we call
-        //callback from parent
+        this.props.onFormSubmit(this.state.term);
     }
 
     render() {
