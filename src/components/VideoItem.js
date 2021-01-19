42fdd0eb487621d9f3e591f4c95ca8bac4abaa60
Varun Kumar <heysadboy@gmail.com>
2021-01-19 09:40:56 +0900

    Completing the video app

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,7 +1,20 @@
+import './VideoItem.css';
 import React from 'react';
 
-const VideoItem = (props) => {
-    return <div>VideoItem</div>;
+const VideoItem = ({ video, onVideoSelect }) => {
+    return (
+        <div onClick={() => onVideoSelect(video)} className="video-item item">
+            <img
+                className="ui image"
+                alt={video.snippet.title}
+                src={video.snippet.thumbnails.medium.url}
+            />
+            <div className="content">
+                <div className="header">
+                    {video.snippet.title}
+                </div>
+            </div>
+        </div>);
 
 };
 
